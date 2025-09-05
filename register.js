1ac4bd26c531109afdf255ccc6107237ae8e38d5
yuvikadessai <yuvikadessai05@gmail.com>
2025-09-05 19:11:54 +0530

    updated file

--- register.js
+++ register.js
@@ -1,3 +1,22 @@
+// Sidebar toggle
+const sidebar = document.getElementById("sidebar");
+const menuBtn = document.getElementById("menuBtn");
+
+menuBtn.addEventListener("click", (e) => {
+  e.stopPropagation(); // stop the click from reaching window
+  sidebar.style.width = "250px";
+});
+
+// Close sidebar if clicked outside
+window.addEventListener("click", (e) => {
+  if (sidebar.style.width === "250px" && !sidebar.contains(e.target) && e.target !== menuBtn) {
+    sidebar.style.width = "0";
+  }
+});
+
+
+
+
 const form = document.getElementById('registerForm');
 const message = document.getElementById('message');
 
@@ -25,3 +44,7 @@ form.addEventListener('submit', function(e) {
         message.textContent = "Error: " + err;
     });
 });
+
+function nextPage(){
+    window.location.href = "todo.html";
+}
